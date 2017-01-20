6e18b7eb37dad97e17a5a94c972f32a0e0de5f57
Tom Gobich <fallofmars@gmail.com>
2017-01-20 17:05:33 -0500

    initial commit - basic auth working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import "rxjs/Rx";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  questions: FirebaseListObservable<any>;
+  usersList: any = null;
+  user: any = null;
+
+  constructor(private af: AngularFire) {
+    // Load questions from database
+    this.questions = af.database.list('/questions', {
+      query : {
+        limitToLast: 20,
+        sortBy: 'postDate'
+      }
+    })
+
+    // Get collection of users to push / pull from
+    this.usersList = af.database.list('/users');
+
+    // Load currently authenticated user
+    af.auth.subscribe(auth => {
+      auth ? this.user = auth : null;
+    })
+  }
+
+  login() {
+    let provider = AuthProviders.Google;
+    let method = AuthMethods.Popup;
+    this.af.auth
+      .login({ provider, method })
+      .then(authUser => {
+        this.af.database.list(`/users/${this.user.uid}`)
+          .subscribe(foundUser => {
+            console.log({foundUser});
+            if(foundUser.length > 0) {
+              // User was found
+              alert('user already exists');
+            }
+            else {
+              // User was not found
+              let newUser = {
+                uid: this.user.uid,
+                displayName: this.user.google.displayName,
+                joinDate: new Date(),
+              }
+              this.af.database.object(`/users/${this.user.uid}`).set(newUser);
+            }
+          })
+      })
+  }
+
+  logout() {
+    this.af.auth.logout().then(() => {
+      this.user = null;
+      console.warn('User has logged out');
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCM_gEezgMewcM20fvLj4iduFAZpqYF3_g",
+  authDomain: "inquism-8e3e6.firebaseapp.com",
+  databaseURL: "https://inquism-8e3e6.firebaseio.com",
+  storageBucket: "inquism-8e3e6.appspot.com",
+  messagingSenderId: "421979106846"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
